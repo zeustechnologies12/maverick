3d911a8a88d467121f06514a2905af69c96797cd
Khunshan <khunshankhalid85@gmail.com>
2024-10-08 02:21:48 +0500

    Added recat native elements

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,24 @@
 import { Stack } from "expo-router";
 
+import { ThemeProvider, createTheme } from "@rneui/themed";
+
+const theme = createTheme({
+  mode: "light",
+  spacing: {
+    xs: 4,
+    sm: 8,
+    md: 16,
+    lg: 32,
+    xl: 64,
+  },
+});
+
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    <ThemeProvider theme={theme}>
+      <Stack>
+        <Stack.Screen name="index" />
+      </Stack>
+    </ThemeProvider>
   );
 }
